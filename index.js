9d67cf450e3ec64620d50f308bd44056ea827feb
Sebastián Osorio <im@svzosorio.co>
2015-11-23 23:08:35 -0600

    use booljs-mongoose as db driver

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var booljs = require('bool.js');
 
 // Here is where magic happens
-booljs('co.svzosorio.ktp')
+booljs('co.svzosorio.ktp', ['booljs-mongoose'])
     .setServerLoader('booljs-express')
-    .setDatabaseLoader('booljs-nomodel')
+    .setDatabaseLoader('booljs-mongoose')
     .run();
